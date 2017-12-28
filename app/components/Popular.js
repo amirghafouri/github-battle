c84fa419226d0bea7fe6cf7ec30b5b8e7aa2af05
Amir Ghafouri <amirfghafouri@gmail.com>
2017-12-27 21:51:02 -0500

    adds event handler to update state when different language selected

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,14 +1,32 @@
 var React = require('react')
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState(function() {
+      return {
+        selectedLanguage: lang
+      }
+    })
+  }
+
   render() {
     var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
     return (
       <div>
+        <p>selectedLanguage: {this.state.selectedLanguage}</p>
         <ul className='languages'>
-          {languages.map(function(lang) {
-            return <li>{lang}</li>
+          {languages.map((lang) => {
+            return <li onClick={this.updateLanguage.bind(null, lang)} key={lang}>{lang}</li>
           })}
         </ul>
       </div>
