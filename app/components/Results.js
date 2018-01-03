e50311fa160a65e04953180fc09a28b2c7c209ed
Amir Ghafouri <amirfghafouri@gmail.com>
2018-01-03 17:52:30 -0500

    makes PlayerPreview component reusable by depending on props.children

--- app/components/Results.js
+++ app/components/Results.js
@@ -3,12 +3,36 @@ var PropTypes = require('prop-types')
 var queryString = require('query-string')
 var api = require('../utils/api')
 var { Link } = require('react-router-dom')
+var PlayerPreview = require('./PlayerPreview')
+
+function Profile(props) {
+  var info = props.info
+
+  return (
+    <PlayerPreview avatar={info.avatar_url} username={info.login}>
+      <ul className="space-list-items">
+        {info.name && <li>{info.name}</li>}
+        {info.children && <li>{info.location}</li>}
+        {info.company && <li>{info.company}</li>}
+        <li>Followers: {info.followers}</li>
+        <li>Following: {info.following}</li>
+        <li>Public Repos: {info.public_repos}</li>
+        {info.blog && (
+          <li>
+            <a href={info.blog}>{info.blog}</a>
+          </li>
+        )}
+      </ul>
+    </PlayerPreview>
+  )
+}
 
 function Player(props) {
   return (
     <div>
-      <h1 className='header'>{props.label}</h1>
-      <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
+      <h1 className="header">{props.label}</h1>
+      <h3 style={{ textAlign: 'center' }}>Score: {props.score}</h3>
+      <Profile info={props.profile} />
     </div>
   )
 }
@@ -34,15 +58,12 @@ class Results extends React.Component {
   componentDidMount() {
     var players = queryString.parse(this.props.location.search)
 
-    api.battle([
-      players.playerOneName,
-      players.playerTwoName
-    ])
-    .then(results => {
+    api.battle([players.playerOneName, players.playerTwoName]).then(results => {
       if (results === null) {
         return this.setState(() => {
           return {
-            error: 'Looks like there was an error. Check that both users exist on Github.',
+            error:
+              'Looks like there was an error. Check that both users exist on Github.',
             loading: true
           }
         })
@@ -60,10 +81,10 @@ class Results extends React.Component {
   }
 
   render() {
-    var error = this.state.error;
-    var winner = this.state.winner;
-    var loser = this.state.loser;
-    var loading = this.state.loading;
+    var error = this.state.error
+    var winner = this.state.winner
+    var loser = this.state.loser
+    var loading = this.state.loading
 
     if (loading === true) {
       return <p>Loading</p>
@@ -72,23 +93,15 @@ class Results extends React.Component {
       return (
         <div>
           {error}
-          <Link to='/battle'>Reset</Link>
+          <Link to="/battle">Reset</Link>
         </div>
       )
     }
 
     return (
-      <div className='row'>
-        <Player
-          label='Winner'
-          score={winner.score}
-          profile={winner.profile}
-        />
-        <Player
-          label='Loser'
-          score={loser.score}
-          profile={loser.profile}
-        />
+      <div className="row">
+        <Player label="Winner" score={winner.score} profile={winner.profile} />
+        <Player label="Loser" score={loser.score} profile={loser.profile} />
       </div>
     )
   }
